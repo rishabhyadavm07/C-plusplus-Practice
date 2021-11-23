5116210b7b8bc20b30d51573f94e70539dad8dd4
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-23 12:28:38 +0530

    More Practice

---
+++ arrays.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int arr[5]={5,6,3,7,3};
+    cout<<arr[4]<<endl;
+    
+    int arr2[5];
+    cout << "Enter the numbers you want to store in the array:"<<endl;
+    
+    
+    for (int i = 0; i < 5; i++) {
+        cin>>arr2[i];
+    }
+
+    cout<<arr2[3]<<endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
