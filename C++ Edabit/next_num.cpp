759e5f565ebfac5c7f16ebcfa5ee5201380cd075
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-23 23:08:10 +0530

    More Practice

---
+++ C++ Edabit/next_num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int integer,plusone;
+    cout<<"Enter a Integer: "<<endl;
+    cin>>integer;
+    plusone=integer+1;
+    cout<<"The number that comes after the provided integer is: "<<plusone<<endl;
+
+
+
+
+
+    return 0;
+}
