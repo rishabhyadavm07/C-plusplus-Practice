f60fe19228a7369de27f4dc1c200ff643b50ce40
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-25 23:38:26 +0530

    More Practice

---
+++ Functions_basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void printmsg(){
+
+    cout<<"Hello ,Program Running!"<<endl;
+}
+int main(){
+    
+    printmsg();
+    cout<<"Program ends"<<endl;
+    return 0;
+}
