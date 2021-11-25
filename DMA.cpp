c6b305a7923225eeca2d7652fdac5be2e12558ca
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-25 22:49:25 +0530

    More Practice

---
+++ DMA.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Program start"<<endl;
+
+    int size;
+    int *ptr;
+    cout<<"Enter the number of inputs you want to give: "<<endl;
+    cin>>size;
+    ptr = new int[size];
+    cout<<"Enter the values you want to store in array : "<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        
+        cin>>ptr[i];
+    }
+    cout<<"The values that are going to be stored in array are: "<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<ptr[i]<<endl;
+    }
+    
+
+
+
+
+
+    cout<<"Program end"<<endl;
+    return 0;
+}
