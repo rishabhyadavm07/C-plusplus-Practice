e974f7c0611aaac795bcb4eab32154e651082c7d
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-29 13:11:22 +0530

    More Practice Files

---
+++ C++ Programs/Passarguments.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void Passbyvalue(int x,int y){
+    int z=x;
+    x=y;
+    y=z;
+}
+void Passbyrefrence(int &x,int &y){
+    int z=x;
+    x=y;
+    y=z;
+}
+void Passbypointer(int *x,int *y){
+    int z=*x;
+    *x=*y;
+    *y=z;
+}
+int main(){
+    int a=4,b=7;
+    cout<<"Before swapping"<<endl<<"a= "<<a<<endl<<"b= "<<b<<endl;
+
+    Passbypointer(&a,&b);
+
+    cout<<"after swapping"<<endl<<"a= "<<a<<endl<<"b= "<<b<<endl;
+    
+    
+    
+    
+    
+    return 0;
+}
