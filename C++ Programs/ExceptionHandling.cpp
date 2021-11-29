1fa2a78eb1c5fb1a3b6888e560b95443447f9832
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-11-29 13:56:37 +0530

    More Practice Files

---
+++ C++ Programs/ExceptionHandling.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int numerator,denominator;
+    float result;
+    cout<<"Enter numerator and Denominator: "<<endl;
+    cin>>numerator>>denominator;
+    try
+    {
+        if (denominator==0)
+        {
+            throw denominator;
+        }
+        result=numerator/denominator; //division takes place here
+    }
+    catch(int ex){
+        cout<<"Division not possible !"<<" You Entered : "<<ex<<endl;
+    }
+    cout<<"The result is: "<<result<<endl;
+    return 0;
+}
