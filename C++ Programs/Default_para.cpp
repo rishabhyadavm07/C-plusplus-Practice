1a509f0a8b31bfebf32b06325f1f08ef7e2ea6b2
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-12-13 22:15:51 +0530

    Updated

--- C++ Programs/Default_para.cpp
+++ C++ Programs/Default_para.cpp
@@ -4,7 +4,7 @@ int add(int a,int b,int c=0,int d=5){
     return(a+b+c+d);
 }
 int main(){
-    cout<<"The sum of 1,2,3,4 is : "<<add(1,2,3,4)<<endl;
+    cout<<"The sum of 1,2,3,4 is  : "<<add(1,2,3,4)<<endl;
     cout<<"The sum of 1,2,0,5 is: "<<add(1,2)<<endl;
 
     return 0;
