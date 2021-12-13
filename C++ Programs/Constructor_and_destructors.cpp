1e3cb765d5cafa4321e27a4717f72c50118ccffe
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-12-13 19:44:47 +0530

    Added more files

---
+++ C++ Programs/Constructor_and_destructors.cpp
@@ -0,0 +1,43 @@
+//we will be using the same file as the oop.cpp in this folder
+#include<iostream>
+#include<string>
+using namespace std;
+class Cars
+{
+private:
+    //member variables
+    string company_name;
+    string model_name;
+    string fuel_type;
+    float milage;
+    double price;
+public:
+//member function
+Cars(){
+    cout<<"default constructer called"<<endl;
+}
+void setdata(string cname,string mname, string ftype, float m, double p){
+    company_name = cname;
+    model_name = mname;
+    fuel_type = ftype;
+    milage = m;
+    price = p;
+};
+void displaydata(){
+    cout<<"These are the car properties:"<<endl;
+    cout<<"comapny name -"<<company_name<<endl;
+    cout<<"model name -"<<model_name<<endl;
+    cout<<"fuel type -"<<fuel_type<<endl;
+    cout<<"milage -"<<milage<<endl;
+    cout<<"price -"<<price<<endl;
+    cout<<"ends"<<endl;
+
+};
+};
+int main(){
+    
+    Cars car1,car2;
+    // car1.setdata("audi","q8","petrol",8.0,2300000); //hardcoding the values instead we can also take input from the users also
+    car1.displaydata();
+    return 0;
+}
