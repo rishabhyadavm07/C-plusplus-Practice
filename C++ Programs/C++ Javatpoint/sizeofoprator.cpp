d0138f6eb140417fddabb908029dc301f2902cc3
Rishabh Yadav <rishabhyadavm07@gmail.com>
2021-12-20 20:01:30 +0530

    Uploaded more practice files

---
+++ C++ Programs/C++ Javatpoint/sizeofoprator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    double c;
+    bool d;
+    cout<<"size of Integer is :"<<sizeof(a)<<endl;
+    cout<<"size of Float is :"<<sizeof(b)<<endl;
+    cout<<"size of Double is :"<<sizeof(c)<<endl;
+    cout<<"size of Boolean is :"<<sizeof(d)<<endl;
+    return 0;
+}
